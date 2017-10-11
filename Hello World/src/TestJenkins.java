79b9c86c4fdb8fb43754d0bd1d89c81bdafa335c
Tayyba23 <10bittfatima@seecs.edu.pk>
2017-10-11 16:34:35 +0500

    Update TestJenkins.java

--- Hello World/src/TestJenkins.java
+++ Hello World/src/TestJenkins.java
@@ -4,6 +4,7 @@ public class TestJenkins {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World to Jenkins");
+		System.out.println("Direct commit from site");
 	}
 
 }
