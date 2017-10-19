e39dd25821abf9a6779252ba7c465c58c0496f91
Tayyba23 <10bittfatima@seecs.edu.pk>
2017-10-19 14:31:49 +0500

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,1 +1,1 @@
-echo git diff --name-only 6cd840378df227c2f9b7465fd363097290ed678f b1afade06e50664 < test_env.txt
+echo git diff --name-only 6cd840378df227c2f9b7465fd363097290ed678f b1afade06e50664 < env.txt
